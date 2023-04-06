cf5dfd904cdff54d00e4ef34a86f71448d9e40f4
belammuia <belammuia0@gmail.com>
2023-04-06 09:23:44 +0300

    update

--- recipe.js
+++ recipe.js
@@ -1,9 +1,28 @@
-const searchbtn = document.getElementById('get').addEventListener("click" ,getList)
-function getList(){
-let result = document.getElementById('search-btn').value;
- fetch(`http://www.themealdb.com/api/json/v1/1/filter.php?i=${result}`)
- .then(res => res.json())
- .then(data => {
-    console.log(data)
- })
+const searchBtn = document.getElementById('get');
+const mealList = document.querySelector('.meal');
+
+searchBtn.addEventListener('click', getList);
+
+function getList() {
+  let result = document.getElementById('search-btn').value;
+  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${result}`)
+    .then(res => res.json())
+    .then(data => {
+      let html = "";
+      if (data.meals) {
+        data.meals.forEach(meal => {
+          html += `
+            <div class="meal" data-id="${meal.idMeal}">
+            <h3>${meal.strMeal}</h3>
+              <img src="${meal.strMealThumb}" alt="ERROR 404">
+              <a class="lik" href="#">Get recipe</a>
+            </div>
+          `;
+        });
+      } else {
+        html = "No results found";
+      }
+      mealList.innerHTML += html;
+    })
+    .catch(error => console.log(error));
 }
