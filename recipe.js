1da297d3167aa77060941b2fae1050bbb96a4c61
belammuia <belammuia0@gmail.com>
2023-04-06 12:25:03 +0300

    update

--- recipe.js
+++ recipe.js
@@ -12,10 +12,14 @@ function getList() {
       if (data.meals) {
         data.meals.forEach(eatup => {
           html += `
+          <div class ="food">
+          <div clss ="foo"
             <div class="meal" data-id="${eatup.idMeal}">
             <h3>${eatup.strMeal}</h3>
               <img src="${eatup.strMealThumb}" alt="ERROR 404">
               <a class="lik" href="#">Get recipe</a>
+            </div>
+            </div>
             </div>
           `;
         });
